2f78ecba4fd5c4a9ac6bb219c176032eacf448f7
MK_Meero <94835099+Godofbacon1@users.noreply.github.com>
2026-08-27 01:20:37 +0300

    added icons

--- source/states/CreditsState.hx
+++ source/states/CreditsState.hx
@@ -39,12 +39,12 @@ class CreditsState extends MusicBeatState
 
 		var defaultList:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
 			["Danny And SpriteBam Team"],
-			["b3plush", "b3", "Main Developer of the Mod", "", "FF0000"],
-			["amartistrandom", "amartistrandom", "Menu Background Artist", "", "FFFFFF"],
-			["bobgaming34", "bobgaming", "SoftCode Scripter", "", "FF6865"],
-			["daybreak", "placeholder", "Composer of high effort", "", "000000"],
+			["b3plush", "b3", "Main Developer of the Mod", "https://youtube.com/@bottle_b3", "FF0000"],
+			["amartistrandom", "amartistrandom", "Menu Background Artist", "https://x.com/AMartistrandom", "FFFFFF"],
+			["bobgaming34", "bobgaming", "SoftCode Scripter", "https://x.com/BobGaming34", "FF6865"],
+			["daybreak", "daybreak", "Composer of high effort", "", "000000"],
 			["jello", "testai", "Artist of Meldo and it's background", "", "00FF00"],
-			["Meero", "placeholder", "Source Coder of the Mod", "", "0000FF"]
+			["Meero", "mkmeero", "Source Coder of the Mod", "https://youtube.com/@mk_meero", "0000FF"]
 			// ["Psych Engine Team"],
 			// ["Shadow Mario",		"shadowmario",		"Main Programmer and Head of Psych Engine",					"https://ko-fi.com/shadowmario",	"444444"],
 			// ["Riveren",				"riveren",			"Main Artist/Animator of Psych Engine",						"https://x.com/riverennn",			"14967B"],

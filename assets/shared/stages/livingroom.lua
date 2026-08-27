b3595f8f58dc1bdbf73bfe5121d2720c28ef7f6c
bottleb3 <bbottle953@gmail.com>
2026-08-27 13:19:17 +0100

    Add files via upload

--- assets/shared/stages/livingroom.lua
+++ assets/shared/stages/livingroom.lua
@@ -1,9 +1,13 @@
 function onCreate()
 	-- background shit
-	makeLuaSprite('stages/canon/mainweek/livingroom', 'stages/canon/mainweek/livingroom', -400, -30);
-	setScrollFactor('stages/canon/mainweek/livingroom', 0.9, 0.9);
+	makeLuaSprite('stages/canon/mainweek/livingroomback', 'stages/canon/mainweek/livingroomback', -400, -30);
+	setScrollFactor('stages/canon/mainweek/livingroomback', 0.9, 0.9);
 
-	addLuaSprite('stages/canon/mainweek/livingroom', false);
+	makeLuaSprite('stages/canon/mainweek/livingroomfront', 'stages/canon/mainweek/livingroomfront', -400, -30);
+	setScrollFactor('stages/canon/mainweek/livingroomfront', 1.0, 1.0);
+
+	addLuaSprite('stages/canon/mainweek/livingroomback', false);
+	addLuaSprite('stages/canon/mainweek/livingroomfront', false);
 	
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
 end

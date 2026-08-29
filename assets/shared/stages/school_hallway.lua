01eb1c4b7bab572636fe2cb9aa9010672f61ab38
bottleb3 <bbottle953@gmail.com>
2026-08-29 17:23:03 +0100

    Update school_hallway.lua

--- assets/shared/stages/school_hallway.lua
+++ assets/shared/stages/school_hallway.lua
@@ -1,11 +1,11 @@
 function onCreate()
 	-- background shit
-	makeLuaSprite('stages\funlearn\school_hallway', 'stages\funlearn\school_hallway', -400, 10);
-	setScrollFactor('stages\funlearn\school_hallway', 1.0, 1.0);
+	makeLuaSprite('stages/funlearn/school_hallway', 'stages/funlearn/school_hallway', -400, 10);
+	setScrollFactor('stages/funlearn/school_hallway', 1.0, 1.0);
 
 	
 
-	addLuaSprite('stages\funlearn\school_hallway', false);
+	addLuaSprite('stages/funlearn/school_hallway', false);
 	
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
 end

8ddb63630d30c608e94c07111e91dbadc7fdef46
bottleb3 <bbottle953@gmail.com>
2026-08-29 10:54:24 +0100

    Add files via upload

--- assets/shared/stages/outsidepool.lua
+++ assets/shared/stages/outsidepool.lua
@@ -1,10 +1,16 @@
 function onCreate()
 	-- background shit
-	makeLuaSprite('stages/canon/freeplay/outsidepool', 'stages/canon/freeplay/outsidepool', -600, -300);
-	setScrollFactor('stages/canon/freeplay/outsidepool', 1.0, 1.0);
-	
+	-- makeLuaSprite('stages/the-unused-stage-scrapyard/outsidepoolback', 'stages/the-unused-stage-scrapyard/outsidepoolback', -500, -300);
+	makeAnimatedLuaSprite('stages/the-unused-stage-scrapyard/outsidepoolback', 'stages/the-unused-stage-scrapyard/outsidepoolback', -500, -300);
+	setScrollFactor('stages/the-unused-stage-scrapyard/outsidepoolback', 1.0, 1.0);
+	luaSpriteAddAnimationByPrefix('stages/the-unused-stage-scrapyard/outsidepoolback', 'outsidepoolback', 'outsidepoolback', 24, true);
+	scaleObject('stages/the-unused-stage-scrapyard/outsidepoolback', 1.2, 1.2);
 
-	addLuaSprite('stages/canon/freeplay/outsidepool', false);
+	makeLuaSprite('stages/the-unused-stage-scrapyard/outsidepoolfront', 'stages/the-unused-stage-scrapyard/outsidepoolfront', -600, -300);
+	setScrollFactor('stages/the-unused-stage-scrapyard/outsidepoolfront', 1.0, 1.0);
+	
+	addLuaSprite('stages/the-unused-stage-scrapyard/outsidepoolback', false);
+	addLuaSprite('stages/the-unused-stage-scrapyard/outsidepoolfront', false);
 	
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
 end
--- assets/shared/stages/school_hallwaychase.lua
+++ assets/shared/stages/school_hallwaychase.lua
@@ -1,10 +1,9 @@
 function onCreate()
 	-- background shit
-	makeLuaSprite('stages/funlearn/school_hallwaychase', 'stages/funlearn/school_hallwaychase', -400, 10);
+	-- makeLuaSprite('stages/funlearn/school_hallwaychase', 'stages/funlearn/school_hallwaychase', -400, 10);
+	makeAnimatedLuaSprite('stages/funlearn/school_hallwaychase', 'stages/funlearn/school_hallwaychase', -400, 10);
 	setScrollFactor('stages/funlearn/school_hallwaychase', 1.0, 1.0);
-
-	makeAnimatedLuaSprite('stages/funlearn/school_hallwaychase', 'stages/funlearn/school_hallwaychase', -400, 10)
-	luaSpriteAddAnimationByPrefix('stages/funlearn/school_hallwaychase', 'stages/funlearn/school_hallwaychase', 'stages/funlearn/school_hallwaychase idle', 24, true);
+	luaSpriteAddAnimationByPrefix('stages/funlearn/school_hallwaychase', 'school_hallwaychase', 'school_hallwaychase idle', 24, true);
 	
 
 	addLuaSprite('stages/funlearn/school_hallwaychase', false);

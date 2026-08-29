321687ef5416ed488597de9ffd1636003d6e2a7f
bottleb3 <bbottle953@gmail.com>
2026-08-29 16:25:45 +0100

    Add files via upload

--- assets/shared/stages/outsidepool.lua
+++ assets/shared/stages/outsidepool.lua
@@ -1,16 +1,16 @@
 function onCreate()
 	-- background shit
-	-- makeLuaSprite('stages/the-unused-stage-scrapyard/outsidepoolback', 'stages/the-unused-stage-scrapyard/outsidepoolback', -500, -300);
-	makeAnimatedLuaSprite('stages/the-unused-stage-scrapyard/outsidepoolback', 'stages/the-unused-stage-scrapyard/outsidepoolback', -500, -300);
-	setScrollFactor('stages/the-unused-stage-scrapyard/outsidepoolback', 1.0, 1.0);
-	luaSpriteAddAnimationByPrefix('stages/the-unused-stage-scrapyard/outsidepoolback', 'outsidepoolback', 'outsidepoolback', 24, true);
-	scaleObject('stages/the-unused-stage-scrapyard/outsidepoolback', 1.2, 1.2);
+	-- makeLuaSprite('stages/canon/freeplay/outsidepoolback', 'stages/canon/freeplay/outsidepoolback', -500, -300);
+	makeAnimatedLuaSprite('stages/canon/freeplay/outsidepoolback', 'stages/canon/freeplay/outsidepoolback', -500, -300);
+	setScrollFactor('stages/canon/freeplay/outsidepoolback', 1.0, 1.0);
+	luaSpriteAddAnimationByPrefix('stages/canon/freeplay/outsidepoolback', 'outsidepoolback', 'outsidepoolback', 24, true);
+	scaleObject('stages/canon/freeplay/outsidepoolback', 1.2, 1.2);
 
-	makeLuaSprite('stages/the-unused-stage-scrapyard/outsidepoolfront', 'stages/the-unused-stage-scrapyard/outsidepoolfront', -600, -300);
-	setScrollFactor('stages/the-unused-stage-scrapyard/outsidepoolfront', 1.0, 1.0);
+	makeLuaSprite('stages/canon/freeplay/outsidepoolfront', 'stages/canon/freeplay/outsidepoolfront', -600, -300);
+	setScrollFactor('stages/canon/freeplay/outsidepoolfront', 1.0, 1.0);
 	
-	addLuaSprite('stages/the-unused-stage-scrapyard/outsidepoolback', false);
-	addLuaSprite('stages/the-unused-stage-scrapyard/outsidepoolfront', false);
+	addLuaSprite('stages/canon/freeplay/outsidepoolback', false);
+	addLuaSprite('stages/canon/freeplay/outsidepoolfront', false);
 	
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
 end
